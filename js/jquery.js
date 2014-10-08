c5a280b32d45ee62bce7181aca94320b15092b6c
Stefan Redfield <stefanredfield@Stefans-MacBook-Air-3.local>
2014-10-08 16:53:14 -0700

    Initial commit

---
+++ js/jquery.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	$('.headline-container ul li:nth-child(1)').click(function(event) {
+    // Preventing default action of the event
+    event.preventDefault();
+    // Getting the height of the document
+    var n = $(document).height();
+    $('html, body').animate({ scrollTop: 100 }, 200);
+//                                       |    |
+//                                       |    --- duration (milliseconds) 
+//                                       ---- distance from the top
+	});
+
+	$('.headline-container ul li:nth-child(2)').click(function(event){
+		event.preventDefault();
+		var n = $(document).height();
+		$('html, body').animate({scrollTop: 800}, 400);
+	});
+
+	$('.headline-container ul li:nth-child(3)').click(function(event){
+		event.preventDefault();
+		var n = $(document).height();
+		$('html, body').animate({scrollTop: 1500}, 400);
+	});
+
+
+})
+
