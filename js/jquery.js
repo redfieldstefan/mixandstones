dc7ac8b79cff2f9b16d4aae960cd7bf63816fbd2
Stefan Redfield <redfield.stefan@gmail.com>
2014-10-26 12:41:27 -0700

    Changes to home menu/ingredients
    Minor design changes.

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$('.headline-container ul li:nth-child(1)').click(function(event) {
+	$('.headline-container ul li:nth-child(2)').click(function(event) {
     // Preventing default action of the event
     event.preventDefault();
     // Getting the height of the document
@@ -11,17 +11,27 @@ $(document).ready(function(){
 //                                       ---- distance from the top
 	});
 
-	$('.headline-container ul li:nth-child(2)').click(function(event){
+	$('.headline-container ul li:nth-child(3)').click(function(event){
 		event.preventDefault();
 		var n = $(document).height();
 		$('html, body').animate({scrollTop: 800}, 400);
 	});
 
-	$('.headline-container ul li:nth-child(3)').click(function(event){
+	$('.headline-container ul li:nth-child(4)').click(function(event){
 		event.preventDefault();
 		var n = $(document).height();
 		$('html, body').animate({scrollTop: 1500}, 400);
 	});
+
+	// RECIPE PAGE//
+
+	$('.recipe-button').click(function(event){
+		event.preventDefault();
+		var n = $(document).height();
+		$('html, body').animate({scrollTop: 750}, 400);
+	});
+
+
 
 
 })
