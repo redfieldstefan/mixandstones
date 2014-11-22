bbdcd8f98f29beae2ce88fc58fc6aaba6b5e17fe
David Davidson <daviddavidson.1988@gmail.com>
2014-11-21 22:01:00 -0800

    Comment out config for a sec

--- app/app.js
+++ app/app.js
@@ -1,16 +1,16 @@
-'use strict';
+// 'use strict';
 
-require('angular/angular');
-require('angular/angular-route');
+// require('angular/angular');
+// require('angular/angular-route');
 
-var mixAndStones = angular.module('mixAndStones', [ 'ngRoute' ]);
+// var mixAndStones = angular.module('mixAndStones', [ 'ngRoute' ]);
 
-require('./controllers/adminController')(mixAndStones);
+// require('./controllers/adminController')(mixAndStones);
 
-mixAndStones.config([ '$routeProvider', function($routeProvider) {
-  $routeProvider
-  .when('/admin', {
-    controller: adminController,
-    view: views/adminView.html
-  });
-}]);
+// mixAndStones.config([ '$routeProvider', function($routeProvider) {
+//   $routeProvider
+//   .when('/admin', {
+//     controller: adminController,
+//     view: views/adminView.html
+//   });
+// }]);
