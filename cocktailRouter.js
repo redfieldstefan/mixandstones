7d739f82669e2ce28b95dd14f94aa9f501ae35bc
David Davidson <daviddavidson.1988@gmail.com>
2014-11-22 16:40:00 -0800

    Update API to match individual cocktail GET requests by string, not ID

--- cocktailRouter.js
+++ cocktailRouter.js
@@ -7,13 +7,16 @@ module.exports = function(app) {
   var apiUrl = '/api/cocktails/';
 
   /**
-   * Create (POST)
+   * Saves a new cocktail in the database
+   *
+   * @param {Object} req.body   The new cocktail
+   * @returns {Object}          The cocktail as saved in the db
    */
 
   app.post(apiUrl, function(req, res) {
     var cocktail = new Cocktail({
       name: req.body.name,
-      ingredients: req.body.ingredients,
+      ingredients: req.body.ingredients.sort(),
       url: req.body.name
         .toLowerCase()
         .replace(/\s/g, '-')
@@ -27,44 +30,56 @@ module.exports = function(app) {
   });
 
   /**
-   * Read (GET)
+   * Returns a single cocktail from the db
+   *
+   * @param {String} req.params.cocktail    The URL-formatted cocktail name
+   * @returns {Object}                      The matching cocktail
    */
 
-  app.get(apiUrl + ':cocktailId', function(req, res) {
-    Cocktail.findOne({ _id: req.params.cocktailId }, function(err, dbResponse) {
-      if (err) {
-        return res.status(500).json(err);
-      }
-      return res.status(200).json(dbResponse);
-    });
+  app.get(apiUrl + ':cocktail', function(req, res) {
+    Cocktail.findOne({ url: req.params.cocktail }, 
+      function(err, dbResponse) {
+        if (err) {
+          return res.status(500).json(err);
+        }
+        return res.status(200).json(dbResponse);
+      });
   });
 
   /**
-   * Search (GET)
+   * Returns an array of cocktails whose ingredients match those
+   * the user has available
+   *
+   * @param {Array} req.body.ingredients    The available ingredients
+   * @returns {Array}                       All matching cocktails
    */
 
   app.get('/api/search', function(req, res) {
-    Cocktail.find({ ingredients: req.body.ingredients }, function(err, dbResponse) {
-      if (err) {
-        return err;
-      }
-      return res.status(200).json(dbResponse);
-    });
+    Cocktail.find({ ingredients: req.body.ingredients.sort() }, 
+      function(err, dbResponse) {
+        if (err) {
+          return err;
+        }
+        return res.status(200).json(dbResponse);
+      });
   });
 
   /**
-   * Update (PUT)
+   * Updates a single cocktail
+   *
+   * @param {ID} req.params.id    The db-generated ID to match on
+   * @returns {Object}            The updated cocktail
    */
 
-  app.put(apiUrl + ':cocktailId', function(req, res) {
+  app.put(apiUrl + ':id', function(req, res) {
     var updatedDrink = {
       name: req.body.name,
-      ingredients: req.body.ingredients,
+      ingredients: req.body.ingredients.sort(),
       url: req.body.name
         .toLowerCase()
         .replace(/\s/g, '-')
     };
-    Cocktail.findOneAndUpdate({ _id: req.params.cocktailId }, 
+    Cocktail.findOneAndUpdate({ _id: req.params.id }, 
       updatedDrink, 
       function(err, dbResponse) {
         if (err) {
@@ -75,11 +90,14 @@ module.exports = function(app) {
   });
 
   /**
-   * Delete (DELETE)
+   * Deletes a cocktail
+   *
+   * @param {ID} req.params.id      The ID of the cocktail to delete
+   * @returns {Object}              A success message
    */
 
-  app.delete(apiUrl + ':cocktailId', function(req, res) {
-    Cocktail.remove({ _id: req.params.cocktailId }, function(err) {
+  app.delete(apiUrl + ':id', function(req, res) {
+    Cocktail.remove({ _id: req.params.id }, function(err) {
       if (err) {
         return res.status(500).json(err);
       }
